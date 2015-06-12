2f83778472424e7c4b24540415b7004f38e9a665
seon <piaoshihuang@wanleyun.com>
2015-06-13 01:13:53 +0800

    update

--- modules/beima_signup_form/model.js
+++ modules/beima_signup_form/model.js
@@ -2,7 +2,7 @@ define(["libs/client/models/base"], function(Base) {
   var Model = Base.extend({
     db: 'firstre',
     collection: 'beima_postulant',
-    prefix: 'http://api.beima.nyouhui.com/data'
+    prefix: 'http://api_firstre.dev.wanleyun.com/data'
   });
   return Model;
 });
