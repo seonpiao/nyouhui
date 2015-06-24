9801254f33af4d8189e88b54fe9fc6a9f79b671d
seon <piaoshihuang@wanleyun.com>
2015-06-24 13:26:21 +0800

    baidu tongji

---
+++ modules/beima_baidu_tongji/index.js
@@ -0,0 +1,9 @@
+define([ "modules/beima_baidu_tongji/view" ], function(View) {
+  return {
+    init: function(el) {
+      var view = new View({
+        el: el
+      });
+    }
+  };
+});
---
+++ modules/beima_baidu_tongji/view.js
@@ -0,0 +1,6 @@
+define([ "libs/client/views/base" ], function(Base) {
+  var View = Base.extend({
+    moduleName: "beima_baidu_tongji"
+  });
+  return View;
+});
