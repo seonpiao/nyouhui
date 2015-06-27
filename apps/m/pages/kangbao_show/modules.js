e2e37a66a4c99fa126200dcbbe6bcf235e03a956
seon <piaoshihuang@wanleyun.com>
2015-06-28 02:49:32 +0800

    update

--- apps/m/pages/kangbao_show/modules.js
+++ apps/m/pages/kangbao_show/modules.js
@@ -1,6 +1,5 @@
-define(["modules/moduleRunner/index", "modules/weixin_uploadimg/index", "modules/kangbao_show/index"], function(ModuleRunner, sha1, weixin_uploadimg, kangbao_show) {
+define(["modules/moduleRunner/index", "modules/weixin_uploadimg/index", "modules/kangbao_show/index"], function(ModuleRunner, weixin_uploadimg, kangbao_show) {
   var modules = {
-    sha1: sha1,
     weixin_uploadimg: weixin_uploadimg,
     kangbao_show: kangbao_show
   };
