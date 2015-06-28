4cbc42f0a96f03a9d6b578827057bf2df915444e
seon <piaoshihuang@wanleyun.com>
2015-06-28 14:03:37 +0800

    update

--- modules/kangbao_show/view.js
+++ modules/kangbao_show/view.js
@@ -1,4 +1,4 @@
-define(["libs/client/views/base", "libs/client/jquery.scrollify"], function(Base) {
+define(["libs/client/views/base", "libs/client/jquery.scrollify", "libs/client/sha1"], function(Base) {
   var View = Base.extend({
     moduleName: "kangbao_show",
     events: {
@@ -33,20 +33,107 @@ define(["libs/client/views/base", "libs/client/jquery.scrollify"], function(Base
         });
       });
       self.initScroll();
-      wx.onMenuShareTimeline({
-        title: '我在康宝马拉松守护跑友的生命安全，我是第一反应人', // 分享标题
-        link: location.href, // 分享链接
-        imgUrl: 'http://' + __global.base["static"] + 'dist/images/m/150x150.png', //
-        success: self.shareSuccess
-      });
-      wx.onMenuShareAppMessage({
-        title: '第一反应', // 分享标题
-        desc: '我在康宝马拉松守护跑友的生命安全，我是第一反应人',
-        link: location.href, // 分享链接
-        imgUrl: 'http://' + __global.base["static"] + 'dist/images/m/150x150.png', //
-        success: self.shareSuccess
-      });
+      self.getTicket();
     },
+    createNonceStr: function() {
+      return Math.random().toString(36).substr(2, 15);
+    },
+    createTimestamp: function() {
+      return parseInt(new Date().getTime() / 1000) + '';
+    },
+    raw: function(args) {
+      var keys = Object.keys(args);
+      keys = keys.sort()
+      var newArgs = {};
+      keys.forEach(function(key) {
+        newArgs[key.toLowerCase()] = args[key];
+      });
+
+      var string = '';
+      for (var k in newArgs) {
+        string += '&' + k + '=' + newArgs[k];
+      }
+      string = string.substr(1);
+      return string;
+    },
+    getTicket: function() {
+      var self = this;
+      var now = this.createTimestamp();
+      var nonceStr = this.createNonceStr();
+      $.ajax({
+        url: 'http://api.firstre.cn/weixin/ticket',
+        // url:'http://dev.api.tvall.com:8999/video/ticket',
+        success: function(data) {
+          var ticket = data.result.ticket;
+          var ret = {
+            jsapi_ticket: ticket,
+            nonceStr: nonceStr,
+            timestamp: now,
+            url: location.href.replace(/#.*$/, '')
+          };
+          var string = self.raw(ret);
+          var sign = shalUtil.hex_sha1(string);
+          wx.config({
+            debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+            appId: 'wx8cfeb90d2826a007', // 必填，公众号的唯一标识
+            timestamp: now, // 必填，生成签名的时间戳
+            nonceStr: nonceStr, // 必填，生成签名的随机串
+            signature: sign, // 必填，签名，见附录1
+            jsApiList: [
+                'checkJsApi',
+                'onMenuShareTimeline',
+                'onMenuShareAppMessage',
+                'onMenuShareQQ',
+                'onMenuShareWeibo',
+                'hideMenuItems',
+                'showMenuItems',
+                'hideAllNonBaseMenuItem',
+                'showAllNonBaseMenuItem',
+                'translateVoice',
+                'startRecord',
+                'stopRecord',
+                'onRecordEnd',
+                'playVoice',
+                'pauseVoice',
+                'stopVoice',
+                'uploadVoice',
+                'downloadVoice',
+                'chooseImage',
+                'previewImage',
+                'uploadImage',
+                'downloadImage',
+                'getNetworkType',
+                'openLocation',
+                'getLocation',
+                'hideOptionMenu',
+                'showOptionMenu',
+                'closeWindow',
+                'scanQRCode',
+                'chooseWXPay',
+                'openProductSpecificView',
+                'addCard',
+                'chooseCard',
+                'openCard'
+              ] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+          });
+          wx.ready(function() {
+            wx.onMenuShareTimeline({
+              title: '我在康宝马拉松守护跑友的生命安全，我是第一反应人', // 分享标题
+              link: location.href, // 分享链接
+              imgUrl: 'http://' + __global.base["static"] + 'dist/images/m/150x150.png', //
+              success: self.shareSuccess
+            });
+            wx.onMenuShareAppMessage({
+              title: '第一反应', // 分享标题
+              desc: '我在康宝马拉松守护跑友的生命安全，我是第一反应人',
+              link: location.href, // 分享链接
+              imgUrl: 'http://' + __global.base["static"] + 'dist/images/m/150x150.png', //
+              success: self.shareSuccess
+            });
+          });
+        }
+      });
+    }
     shareSuccess: function() {
       alert('分享成功');
     },
