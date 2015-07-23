a6a6b9b79b934c9f7d0b2b9f6577f1f33ad1eeed
seon <piaoshihuang@wanleyun.com>
2015-07-23 17:07:23 +0800

    支持单文件上传 && 增加获取救援详情接口

--- modules/manage_ctrl_fileuploader/view.js
+++ modules/manage_ctrl_fileuploader/view.js
@@ -2,12 +2,15 @@ define(["libs/client/views/base"], function(Base) {
   var View = Base.extend({
     moduleName: "manage_ctrl_fileuploader",
     init: function() {
+      var self = this;
+      this.max = (this.$el.attr('data-max') || Infinity) * 1;
       this.$fileupload = this.$('.fileupload');
       // Initialize the jQuery File Upload widget:
       this.$fileupload.fileupload({
         // Uncomment the following to send cross-domain cookies:
         //xhrFields: {withCredentials: true},
         url: '/api/upload?dir=manage',
+        maxNumberOfFiles: this.max,
         getFilesFromResponse: function(data) {
           data = data.result;
           if (data.code === 200) {
@@ -18,10 +21,12 @@ define(["libs/client/views/base"], function(Base) {
       });
     },
     name: function() {
-      return '';
+      return this.$el.attr('data-name');
     },
     value: function() {
-      return '';
+      return _.map(this.$('.file-url'), function(el) {
+        return $(el).attr('href');
+      });
     }
   });
   return View;
