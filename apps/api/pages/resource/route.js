98852f30eddbc9ce9a194ba33739612bcc0e9597
seon <piaoshihuang@wanleyun.com>
2015-07-27 17:51:26 +0800

    调试上传

--- apps/api/pages/resource/route.js
+++ apps/api/pages/resource/route.js
@@ -5,10 +5,7 @@ var logger = require('log4js').getLogger('resource');
 module.exports = function(app) {
 
   var checkLogin = require('../../util/checkLogin')(app);
-  var uploader = require('koa-bylh-upload')({
-    host: app.config.upload.host,
-    path: app.config.upload.path
-  });
+  var uploader = require('koa-bylh-upload')(app.config.upload);
 
   var route = app.route('/resource');
 
@@ -17,18 +14,31 @@ module.exports = function(app) {
     var uid =
       yield checkLogin.call(this);
     if (!uid) return;
+    var parts = this.parts;
+    var helpId = parts.field.help_id;
+    //1是图片，2是语音，3是视频，0是未知类型
+    var typeMap = {
+      '.jpg': 1,
+      '.jpeg': 1,
+      '.png': 1,
+      '.gif': 1,
+      '.mp4': 3
+    };
     try {
       var result = yield uploader.call(this);
       if (app.config.upload.db && app.config.upload.collection) {
         result.owner = uid;
+        result.type_id = typeMap[result.type] || 0;
+        result.help_id = helpId;
         var db = yield Mongo.get({
           hosts: app.config.mongo.replset.split(','),
           db: app.config.upload.db
         });
         var collection = db.collection(app.config.upload.collection);
-        var inserted = yield thunkify(collection.insert.bind(collection))(result, {
-          fullResult: true
-        });
+        var inserted = yield thunkify(collection.insert.bind(collection))
+          (result, {
+            fullResult: true
+          });
         this.result = {
           code: 0,
           result: inserted.ops[0]
