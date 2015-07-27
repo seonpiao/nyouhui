e8ee2d8aba61d03d8c0a314f3df0c6836b34ee28
seon <piaoshihuang@wanleyun.com>
2015-07-27 21:58:07 +0800

    调整端口

--- apps/config.js
+++ apps/config.js
@@ -30,7 +30,7 @@ var config = {
   },
   apps: {
     api: {
-      port: '9003',
+      port: 9002,
       upload: {
         host: 'static.test.bylh.tv',
         path: '/usr/share/nginx/html/',
@@ -39,11 +39,11 @@ var config = {
     },
     m: {
       name: '北京马拉松',
-      port: '9004'
+      port: 9004
     },
     manage: {
       name: '第一反应',
-      port: '9999',
+      port: 9001,
       socket: {
         port: 3002
       }
