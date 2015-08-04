6b9e257b20eac85dfde01967f372a0207deb4f09
seon <piaoshihuang@wanleyun.com>
2015-08-04 11:07:54 +0800

    获取用户信息接口，直接拼合外部引用数据

--- apps/api/util/getUserById.js
+++ apps/api/util/getUserById.js
@@ -17,6 +17,18 @@ var getUserById = function(app) {
         }
       });
     result = result[app.config.mongo.defaultDB][app.config.mongo.collections.user];
+    var extDatas = yield Mongo.getExtData({
+      collection: app.config.mongo.collections.user,
+      withoutSchema: true
+    });
+    //外部数据的collection名需要跟用户字段名一致
+    extDatas.forEach(function(extData) {
+      for (var db in extData) {
+        for (var collection in extData[db]) {
+          result[collection] = extData[db][collection];
+        }
+      }
+    });
     return result;
   }
 }
