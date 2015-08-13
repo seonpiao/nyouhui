f34a7fcd106ca17b07534bc18d99ce90cd3d0730
seon <piaoshihuang@wanleyun.com>
2015-08-13 18:08:12 +0800

    支持保持文件名

--- modules/manage_ctrl_fileuploader/view.js
+++ modules/manage_ctrl_fileuploader/view.js
@@ -1,15 +1,19 @@
 define(["libs/client/views/base"], function(Base) {
   var View = Base.extend({
     moduleName: "manage_ctrl_fileuploader",
+    events: {
+      'change input[name="keepname"]': 'keepname'
+    },
     init: function() {
       var self = this;
       this.max = (this.$el.attr('data-max') || Infinity) * 1;
       this.$fileupload = this.$('.fileupload');
+      this.url = '/api/upload?dir=manage&keepname=0';
       // Initialize the jQuery File Upload widget:
       this.$fileupload.fileupload({
         // Uncomment the following to send cross-domain cookies:
         //xhrFields: {withCredentials: true},
-        url: '/api/upload?dir=manage',
+        url: this.url,
         maxNumberOfFiles: this.max,
         getFilesFromResponse: function(data) {
           data = data.result;
@@ -19,6 +23,10 @@ define(["libs/client/views/base"], function(Base) {
           }
         }
       });
+    },
+    keepname: function() {
+      var $keepname = this.$('input[name="keepname"]');
+      this.$fileupload.fileupload('option', 'url', '/api/upload?dir=manage&keepname=' + ($keepname.is(':checked') ? '1' : '0'));
     },
     name: function() {
       return this.$el.attr('data-name');
